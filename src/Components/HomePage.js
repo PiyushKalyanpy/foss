ffdefb8b1d2e414b6479f02de749b0e100c2b28f
Deep Roop <deepsinghroop@gmail.com>
2023-03-18 12:30:44 +0530

    navbar created.

---
+++ src/Components/HomePage.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className='mt-5'>
+        <div>
+            <div className='flex justify-between'>
+                <ul className='flex space-x-20 ml-20'>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Swag</li>
+                    <li>Sponsor</li>
+                    <li>Team</li>
+                </ul>
+                <div className='mr-10'>
+                    <a href=''><i className="fa-brands fa-twitter fa-xl pr-5"></i></a>
+                    <a href=''><i className="fa-brands fa-discord fa-xl pr-5"></i></a>
+                    <a href=''><i className="fa-brands fa-linkedin fa-xl pr-5"></i></a>
+
+                </div>
+            </div>
+
+            <div>
+                <h1>TechX Hackathon</h1>
+                <h2>Innovate for good cause</h2>
+            </div>
+
+        </div>
+        
+    </div>
+  )
+}
+
+export default HomePage
