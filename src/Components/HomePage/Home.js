45c75e4f577f8b8adfc1b27f1453af6e05468db8
Deep Roop <deepsinghroop@gmail.com>
2023-03-19 22:05:55 +0530

    .

--- src/Components/HomePage/Home.js
+++ src/Components/HomePage/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Home.css';
-// import bg1 from '../../Images/bg';
+import bg1 from '../../Images/bg.svg';
+import snake from '../../Images/snake.svg'
 function Home() {
   return (
     <div className='mt-5'>
@@ -24,12 +25,17 @@ function Home() {
             <div className=' flex flex-col items-center justify-center h-screen'>
                 <h1 style={{fontSize: "50px"}} >TechX Hackathon</h1>
                 <h2 style={{fontSize: "25px"}} className='mb-10'>Innovate for good cause</h2>
-                <img src='../../Images/bg.svg' alt="Your SVG" />
+                <img src={bg1} alt="Your SVG" />
                 <div className='flex mb-20'>
                     <button className='rounded-none mr-10 border-2 p-2'>Register Now <i className="fa-solid fa-arrow-right fa-l pl-2 pr-2"></i></button>
                     <button className='rounded-none border-2 p-2'> <a href=''><i className="fa-brands fa-discord fa-l pr-5"></i></a>Join Discord</button>
                 </div>
                 <a href=''><i className="fa-solid fa-arrow-down fa-xl pr-5"></i></a>
+            </div>
+
+            <div>
+                <img src={snake} alt="Your SVG" />
+
             </div>
 
         </div>
